06a816d18d01ec18a12037d982dc214ff886f1db
Beast0E4 <ebison.04@gmail.com>
2025-02-19 23:38:08 +0530

    Feat: Final Check

--- server/index.js
+++ server/index.js
@@ -4,9 +4,7 @@ const app = express();
 app.use("/", (req, res) => {
     res.send("Started");
 })
-app.use("/test", (req, res) => {
-    res.send("Ok");
-})
+
 
 app.listen(8080, () => {
     console.log("listning to port 8080!");
