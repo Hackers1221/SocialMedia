a1e8c6e4a5a51818e7d8d715168f9d05c27691e2
Rounak kumar <rounakkumar190504@gmail.com>
2025-02-19 23:30:25 +0530

    checking

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use("/", (req, res) => {
+    res.send("Started");
+})
+
+app.listen(8080, () => {
+    console.log("listning to port 8080!");
+})
