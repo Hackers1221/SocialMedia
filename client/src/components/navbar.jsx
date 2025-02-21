80e6983592ac07fb60dc36feb6356e792ee346a9
Beast0E4 <ebison.04@gmail.com>
2025-02-21 09:58:15 +0530

    Feat: Edited search bar

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -3,9 +3,9 @@ import { FaSearch } from "react-icons/fa";
 
 function Navbar () {
     return (
-        <header className={`py-[0.7rem] header top-0 bg-[${_COLOR.less_light}] max-w-screen shadow-md flex items-center justify-between px-8 py-02`}>
-            <div className="w-[10%]"></div>
-            <div className={`w-[35%] flex items-center border border-[${_COLOR.medium}] rounded-lg`}>
+        <div className={`top-0 bg-[${_COLOR.less_light}] max-w-screen shadow-md flex items-center justify-between px-8 py-2`}>
+            <div className="w-8"></div>
+            <div className={`p-[0.1rem] w-[35%] mr-52 flex items-center border border-[${_COLOR.medium}] rounded-3xl`}>
                 <FaSearch className={`h-full w-10 px-2 py-2 bg-transparent`} />
                 <input 
                     type="text" 
@@ -17,7 +17,7 @@ function Navbar () {
             <div className="flex justify-end">
                 <IoPerson className="w-8 h-8"/>
             </div>
-        </header>
+        </div>
     )
 }
 
