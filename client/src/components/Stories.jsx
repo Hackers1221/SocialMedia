d17ba7c54d40f0c4a3490ea789674e45039c1f57
Beast0E4 <ebison.04@gmail.com>
2025-02-22 01:09:29 +0530

    Feat: Added stories menu

--- client/src/components/Stories.jsx
+++ client/src/components/Stories.jsx
@@ -1,47 +1,47 @@
 function Stories () {
     return (
-        <section className={`fixed top-0 md:top-[3.5rem] md:right-0 bg-[${_COLOR.less_light}] min-h-screen md:flex flex-col items-center pr-4 pt-4`}>
-            <ul className="md:flex-col items-center justify-center md:space-x-8">
-                <li className="flex flex-col items-center space-y-2 bg-yellow-500">
-                    <div className="bg-gradient-to-tr from-yellow-500 to-pink-600 rounded-full p-1">
-                        <a className="block bg-white p-1 rounded-full transform transition hover:-rotate-12 duration-300" href="#">
-                            <img className="h-10 w-10 rounded-full" src="https://i.ibb.co/yhh0Ljy/profile.jpg" alt="image"/>
+        <section class="fixed top-[3.5rem] right-0 bg-transparent min-h-screen flex flex-row md:flex-col items-center pt-5 pr-4">
+            <ul class="flex flex-row md:flex-col items-center justify-center md:space-x-8 gap-2">
+                <li class="flex flex-col items-center space-y-2">
+                    <div class="bg-gradient-to-tr from-yellow-500 to-pink-600 rounded-full p-1 relative">
+                        <a class="block bg-white p-1 rounded-full transform transition hover:-rotate-12 duration-300" href="#">
+                            <img class="h-10 w-10 rounded-full" src="https://i.ibb.co/yhh0Ljy/profile.jpg" alt="image"/>
                         </a>
-                        {/* <button className={`transition duration-500 absolute bottom-0 right-0 bg-[${_COLOR.dark}] h-8 w-8 rounded-full text-white text-2xl font-semibold border-4 border-white flex justify-center items-center hover:bg-blue-900`}>+</button> */}
+                        <button class="transition duration-500 absolute bottom-0 right-0 bg-blue-700 h-8 w-8 rounded-full text-white text-2xl font-semibold border-4 border-white flex justify-center items-center hover:bg-blue-900">+</button>
                     </div>
-                    <p className="text-sm">you</p>
+                    <p className={`text-sm text-[${_COLOR.lightest}]`}>beast0E4</p>
                 </li>
-                <li className="flex flex-col items-center bg-yellow-500 w-full">
-                    <div className="bg-gradient-to-tr from-yellow-500 to-pink-600 rounded-full p-1">
-                        <a className="block bg-white p-1 rounded-full transform transition hover:-rotate-12 duration-300" href="#">
-                            <img className="h-10 w-10 rounded-full" src="https://i.ibb.co/yhh0Ljy/profile.jpg" alt="image"/>
+                <li class="flex flex-col items-center space-y-2">
+                    <div class="bg-gradient-to-tr from-yellow-500 to-pink-600 rounded-full p-1">
+                        <a class="block bg-white p-1 rounded-full transform transition hover:-rotate-12 duration-300" href="#">
+                            <img class="h-10 w-10 rounded-full" src="https://i.ibb.co/yhh0Ljy/profile.jpg" alt="image"/>
                         </a>
                     </div>
-                    <p className="text-sm">dealer_101</p>
+                    <p className={`text-sm text-[${_COLOR.lightest}]`}>dealer_101</p>
                 </li>
-                <li className="flex flex-col items-center space-y-2">
-                    <div className="bg-gradient-to-tr from-yellow-500 to-pink-600 rounded-full p-1">
-                        <a className="block bg-white p-1 rounded-full transform transition hover:-rotate-12 duration-300" href="#">
-                            <img className="h-10 w-10 rounded-full" src="https://i.ibb.co/yhh0Ljy/profile.jpg" alt="image"/>
+                <li class="flex flex-col items-center space-y-2">
+                    <div class="bg-gradient-to-tr from-yellow-500 to-pink-600 rounded-full p-1">
+                        <a class="block bg-white p-1 rounded-full transform transition hover:-rotate-12 duration-300" href="#">
+                            <img class="h-10 w-10 rounded-full" src="https://i.ibb.co/yhh0Ljy/profile.jpg" alt="image"/>
                         </a>
                     </div>
-                    <p className="text-sm">raftaar_086</p>
+                    <p className={`text-sm text-[${_COLOR.lightest}]`}>rounak_086</p>
                 </li>
-                <li className="flex flex-col items-center space-y-2">
-                    <div className="bg-gradient-to-tr from-yellow-500 to-pink-600 rounded-full p-1">
-                        <a className="block bg-white p-1 rounded-full transform transition hover:-rotate-12 duration-300" href="#">
-                            <img className="h-10 w-10 rounded-full" src="https://i.ibb.co/yhh0Ljy/profile.jpg" alt="image"/>
+                <li class="flex flex-col items-center space-y-2">
+                    <div class="bg-gradient-to-tr from-yellow-500 to-pink-600 rounded-full p-1">
+                        <a class="block bg-white p-1 rounded-full transform transition hover:-rotate-12 duration-300" href="#">
+                            <img class="h-10 w-10 rounded-full" src="https://i.ibb.co/yhh0Ljy/profile.jpg" alt="image"/>
                         </a>
                     </div>
-                    <p className="text-sm">forhadx_9008</p>
+                    <p className={`text-sm text-[${_COLOR.lightest}]`}>forhadx_9008</p>
                 </li>
-                <li className="flex flex-col items-center space-y-2">
-                    <div className="bg-gradient-to-tr from-yellow-500 to-pink-600 rounded-full p-1">
-                        <a className="block bg-white p-1 rounded-full transform transition hover:-rotate-12 duration-300" href="#">
-                            <img className="h-10 w-10 rounded-full" src="https://i.ibb.co/yhh0Ljy/profile.jpg" alt="image"/>
+                <li class="flex flex-col items-center space-y-2">
+                    <div class="bg-gradient-to-tr from-yellow-500 to-pink-600 rounded-full p-1">
+                        <a class="block bg-white p-1 rounded-full transform transition hover:-rotate-12 duration-300" href="#">
+                            <img class="h-10 w-10 rounded-full" src="https://i.ibb.co/yhh0Ljy/profile.jpg" alt="image" />
                         </a>
                     </div>
-                    <p className="text-sm">shahnoor_877</p>
+                    <p className={`text-sm text-[${_COLOR.lightest}]`}>shahnoor_877</p>
                 </li>
             </ul>
         </section>
