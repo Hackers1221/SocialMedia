74c493f93f9dfabfe700e9f6835b51d9a2ccc173
Beast0E4 <ebison.04@gmail.com>
2025-02-22 02:15:23 +0530

    Feat: Fixed something

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,14 +1,10 @@
-import Messages from "../components/Messages";
 import Stories from "../components/Stories";
-import PostPage from "./posts/postPage";
 
 function Home () {
 
     return  (
         <>
             <Stories />
-            <Messages />
-            <PostPage />
         </>
     )
 }
