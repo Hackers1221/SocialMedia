ff5f5abb2db5bb407972b4cc74cc88b4394c0d1a
Beast0E4 <ebison.04@gmail.com>
2025-02-25 10:44:28 +0530

    Feat: Modified Explore page

--- client/src/pages/routing/mainRoutes.jsx
+++ client/src/pages/routing/mainRoutes.jsx
@@ -9,7 +9,6 @@ import Explore from "../../components/Explore";
 import Messenger from "../../components/Messenger";
 import Pulse from "../Pulse/Pulse";
 import DisplayStory from "../../components/DisplayStory";
-import ExploreGrid from "../../components/ExploreGrid";
 
 function MainRoutes() {
   return (
@@ -24,7 +23,6 @@ function MainRoutes() {
         <Route path="messenger" element={<Messenger />} />
         <Route path="pulse" element={<Pulse />} />
         <Route path="story" element={<DisplayStory />}/>
-        <Route path="exp" element={<ExploreGrid />}/>
       </Route>
     </Routes>
   );
