66465b009c75bd9c68caab107f522219f6bd8768
Reshab Prasad <prasadreshab84@gmail.com>
2025-02-25 23:39:04 +0530

    Feat: Made some changes

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -76,7 +76,7 @@ const Profile = () => {
                 <h2 className={`text-sm text-[${_COLOR.lightest}]`}>{countFollowing} Following</h2>
               </div>
               {!check && (
-                <button className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" onClick={toggleFollow}>
+                <button className="mt-2 px-2 py-1 bg-gray-200 text-gray-800 rounded hover:bg-gray-400 transition-all duration-300 ease-in-out  hover:cursor-pointer" onClick={toggleFollow}>
                   {follow ? "Unfollow" : "Follow"}
                 </button>
               )}
