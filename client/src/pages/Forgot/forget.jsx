540c30ad3882561bf085c43143963fa7ffd691dd
Rounak kumar <rounakkumar190504@gmail.com>
2025-05-02 23:35:35 +0530

    Fix: file name change

--- client/src/pages/Forgot/forget.jsx
+++ client/src/pages/Forgot/forget.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { forgetPass } from "../../redux/Slices/auth.slice";
-import { toast } from "react-hot-toast";
+import { toast } from "react-hot-toast"; 
 
 function ForgetPassword() {
     const navigate = useNavigate();
