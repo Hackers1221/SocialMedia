41b4f19a0f2083557e19090a68ad52c307a2f526
Rounak Kumar <149093677+Rounak1945@users.noreply.github.com>
2025-05-03 00:42:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -58,14 +58,6 @@ app.use((err,req,res,next) => {
     })
 })
 
-// Serve static files from React app
-app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-
-// Catch-all to return React index.html for unknown routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
-});
-
 async function ConnectToDb(){
 
     try {
